ebae0e327aed20dd0dc59415c9184ef07374901f
julianromero83 <rom16015@byui.edu>
2016-10-08 21:48:14 -0700

    Updated HelloWorld.java -removed World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String argv[]) {
-        System.out.println("Hello GitHub World");
+        System.out.println("Hello GitHub");
+		System.out.println("Testing...");
     }
 }
